5690321d01eb28dcd33ddc6706d323d2e4840109
Dale Diaz <stropheum@yahoo.com>
2015-05-20 22:36:26 -0400

    Isolated Calculation Service (BROKEN)
    Removed speed calculation components from the workout activity and
    placed into a speed calculation service. Much debugging has to be done.

--- app/src/main/java/com/example/stropheum/speedcalculatortest/SpeedAlarmActivity.java
+++ app/src/main/java/com/example/stropheum/speedcalculatortest/SpeedAlarmActivity.java
@@ -1,240 +1,202 @@
 package com.example.stropheum.speedcalculatortest;
 
+import android.content.ComponentName;
 import android.content.Context;
-import android.location.Location;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.os.Vibrator;
-
+import com.example.stropheum.speedcalculatortest.SpeedCalculationService.SpeedCalculationBinder;
 
 public class SpeedAlarmActivity extends ActionBarActivity {
 
+    public SpeedCalculationService speedCalculator;
+    boolean isBound = false;
+
     final int MILLI_TO_SEC = 1000;
     final int SEC_TO_HOUR = 3600;
+
+    double currentPace, goalPace;
+    String paceText;
+
     Vibrator vibrator;
 
     // Allow 15 seconds of error for time calculations
     final double MILE_TIME_ERROR = 0.25;
 
-    LocationManager locationManager;
-    LocationListener locationListener;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        vibrator  = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
-
         setContentView(R.layout.activity_speed_alarm);
 
-        locationManager =
-                (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        //Intent i = new Intent(this, SpeedCalculationService.class);
 
-        locationListener = new LocationListener() {
-            double lonNew, lonOld;
-            double latNew, latOld;
+        //bindService(i, speedConnection, Context.BIND_AUTO_CREATE);
+        //startService(i);
 
-            // Values to track display interval
-            long paceStartTime, paceDeltaTime;
-            long speedStartTime, speedDeltaTime;
+        // Starts the service for calulating user's speed
+        //startService(new Intent(getBaseContext(), SpeedCalculationService.class)); // was below bind before
 
-            // Values to track computation interval
-            long timeNew;
-            long timeOld = System.currentTimeMillis();
-            long deltaTime;
+//        vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+//
+//        double startTime, elapsedTime;
+//        double alertIntervalStart, alertIntervalElapsed;
+//        double speed, goalPace, currentPace;
+//
+//        String paceText = "Waiting for GPS signal";
+//        updatePaceText(paceText);
+//
+//        if (isBound);
+//        // Delays workout until the service finds a signal
+//        //while (!speedCalculator.gpsSignalFound());
+//
+//        // Once GPS connection is established, being the workout
+//        paceText = "Begin!";
+//        updatePaceText(paceText);
+//
+//        ///////////////////
+//        // Part One Begins
+//        /////////////////
+//        startTime = System.currentTimeMillis();
+//        elapsedTime = 0;
+//        alertIntervalStart = startTime; // Initialize 30 second timer on workout start
+//
+//        goalPace = 10.0;
+//        updateGoalPace(goalPace);
+//
+//        do {
+//            // Update time since last alert
+//            alertIntervalElapsed = System.currentTimeMillis() - alertIntervalStart;
+//
+//            //speed = speedCalculator.getCurrentSpeed();
+//            speed = 1;
+//            currentPace = 60 / speed;
+//
+//            // Update speed and pace every second
+//            if (elapsedTime >= 1.0 * MILLI_TO_SEC) {
+//                updateSpeed(speed);
+//                updateCurrentPace(currentPace);
+//            }
+//
+//            // Alerts user if 30 seconds have gone by with no change
+//            if (alertIntervalStart >= 30 * MILLI_TO_SEC) {
+//                paceAlert();
+//                alertIntervalStart = System.currentTimeMillis();
+//            }
+//
+//            // If 5 seconds have elapsed and perfect pace, alert user
+//            if (alertIntervalElapsed >= 5 * MILLI_TO_SEC && checkPace(currentPace, goalPace)) {
+//                paceAlert();
+//                alertIntervalStart = System.currentTimeMillis();
+//            }
+//
+//            elapsedTime = System.currentTimeMillis() - startTime;
+//        } while (elapsedTime < 120 * MILLI_TO_SEC);
+//
+//        paceText = "Workout Complete!";
+//        updatePaceText(paceText);
 
-            double distance, speed, mileTime;
-            double goalMileTime = 2;
-
-            int ticks = 0;
-            double speedSum = 0;
-
-            boolean firstRun = true;
-
-            String paceText;
-
-
-            public void onLocationChanged(Location location) {
-                // Calculate change in time
-                if (firstRun) {
-                    paceStartTime = System.currentTimeMillis();
-                    speedStartTime = System.currentTimeMillis();
-                    //timeOld = System.currentTimeMillis();
-                    lonOld = Math.toRadians(location.getLongitude());
-                    latOld = Math.toRadians(location.getLongitude());
-                    updateGmt(goalMileTime);
-                    paceText = "Begin!";
-                    updatePaceText(paceText);
-                    firstRun = false;
-                }
-                timeNew = System.currentTimeMillis();
-                deltaTime = Math.abs(timeNew - timeOld);
-
-                // Compute new positional coordinates
-                lonNew = Math.toRadians(location.getLongitude());
-                latNew = Math.toRadians(location.getLatitude());
-
-                distance = haversine(latOld, lonOld, latNew, lonNew);
-                if (distance > 1000) { distance = 0; }
-
-                // Add current speed and count number of ticks
-                speedSum += (distance / deltaTime) * MILLI_TO_SEC * SEC_TO_HOUR;
-                ticks++;
-                paceDeltaTime = System.currentTimeMillis() - paceStartTime;
-                speedDeltaTime = System.currentTimeMillis() - speedStartTime;
-
-                if (paceDeltaTime >= 30000) { // Update all values and pace text
-                    speed = speedSum / ticks;
-                    mileTime = 60 / speed;
-
-                    checkPace();
-
-                    // Update values on screen
-                    if (speed > 10000) {
-                        speed = 0;
-                    }
-                    updateSpeed(speed);
-                    updateEmt(mileTime);
-                    updatePaceText(paceText);
-
-                    ticks = 0;
-                    speedSum = 0;
-                    paceStartTime = System.currentTimeMillis();
-                } else if (speedDeltaTime >= 5000) { // Update values without interfering with average
-                    speed = speedSum / ticks;
-                    mileTime = 60 / speed;
-
-                    if (speed > 10000) {
-                        speed = 0;
-                    }
-                    updateSpeed(speed);
-                    updateEmt(mileTime);
-
-                    // If player resumes proper pace, reset interval timer and prompt pace
-                    if (mileTime > goalMileTime - MILE_TIME_ERROR &&
-                        mileTime < goalMileTime + MILE_TIME_ERROR) {
-                        checkPace();
-                        //paceStartTime = System.currentTimeMillis();
-                    }
-
-                    speedStartTime = System.currentTimeMillis();
-                }
-
-            // Store old Coordinates and time
-            latOld = latNew;
-            lonOld = lonNew;
-            timeOld = timeNew;
-
-        }
-
-
-            public void onStatusChanged(String Provider, int status, Bundle extras) {}
-
-            public void onProviderEnabled(String provider) {}
-
-            public void onProviderDisabled(String provider) {}
-
-            /**
-             * Updates the current speed of the user
-             * @param speed the current speed value
-             */
-            private void updateSpeed(double speed) {
-                final TextView speedVal = (TextView) findViewById(R.id.SpeedVal);
-                speedVal.setText(String.format("%.2f", speed));
-            }
-
-            /**
-             * Updates the current estimated mile time
-             * @param mileTime current EMT
-             */
-            private void updateEmt(double mileTime) {
-                int minutes = (int)mileTime;
-                int seconds = (int)(((mileTime * 100) % 100) * 0.6);
-                final TextView emtVal = (TextView) findViewById(R.id.emtVal);
-                emtVal.setText(String.format("%d:%02d", minutes, seconds));
-            }
-
-            /**
-             * Updates the current goal mile time
-             * @param goalMileTime new goal mile time
-             */
-            private void updateGmt(double goalMileTime) {
-                int minutes = (int)goalMileTime;
-                int seconds = (int)(((mileTime * 100) % 100) * 0.6);
-                final TextView gmtVal = (TextView) findViewById(R.id.gmtVal);
-                gmtVal.setText(String.format("%d:%02d", minutes, seconds));
-            }
-
-            /**
-             * Updates the current pace text
-             * @param paceText indicator for user;s current speed in relation to goal time
-             */
-            private void updatePaceText(String paceText) {
-                final TextView pace = (TextView) findViewById(R.id.paceView);
-                pace.setText(paceText);
-            }
-
-            /**
-             * Checks current pace and assigns appropriate text
-             */
-            private void checkPace() {
-                if (mileTime > goalMileTime + MILE_TIME_ERROR) {
-                    paceText = "Speed up";
-                    vibrator.vibrate(300);
-                    try {
-                        Thread.sleep(300);
-                    } catch (Exception e) {}
-                    vibrator.vibrate(300);
-                    try {
-                        Thread.sleep(300);
-                    } catch (Exception e) {}
-                    vibrator.vibrate(300);
-                } else if (mileTime < goalMileTime - MILE_TIME_ERROR) {
-                    paceText = "Slow Down";
-                    vibrator.vibrate(1000);
-                } else {
-                    paceText = "Perfect Pace!";
-                }
-            }
-        };
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+//        ///////////////////
+//        // Part Two Begins
+//        /////////////////
+//        startTime = System.currentTimeMillis();
+//        elapsedTime = 0;
+//        alertIntervalStart = startTime; // Initialize 30 second timer on workout start
+//
+//        goalPace = 6.0;
+//
+//        do {
+//
+//            elapsedTime = System.currentTimeMillis() - startTime;
+//        } while (elapsedTime < 60 * MILLI_TO_SEC);
+//
+//
 
     }
 
     /**
-     * Computes distance (in miles) between two coordinates using the haversine formula
-     * @param lat1 latitude  of previous location
-     * @param lon1 longitude of previous location
-     * @param lat2 latitude  of current  location
-     * @param lon2 longitude of current  location
-     * @return distance in miles
+     * Checks if the user is running in an acceptable range of the goal pace
+     * @param currentPace Current speed of the user
+     * @param goalPace Goal speed of the user
+     * @return True if the pace is acceptable, false otherwise
      */
-    private double haversine(double lat1, double lon1, double lat2, double lon2) {
-        final double EARTH_RADIUS_M = 3959;
-
-        double dLon, dLat, a, c, distance;
-
-        // Calculate distance traveled using Haversine formula
-        dLon = lon2 - lon1;
-        dLat = lat2 - lat1;
-
-        a = Math.sin(dLat/2.0) * Math.sin(dLat/2.0) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon/2.0) * Math.sin(dLon/2.0);
-        System.out.println("a = " + a);
-
-        c = 2.0 * Math.atan(Math.sqrt(a));
-        System.out.println("c = " + c);
-        distance = EARTH_RADIUS_M * c;
-
-        return distance;
+    private boolean checkPace(double currentPace, double goalPace) {
+        boolean result = true;
+        if (currentPace > goalPace + MILE_TIME_ERROR || currentPace < goalPace - MILE_TIME_ERROR) {
+            result = false;
+        }
+        return result;
     }
 
+    /**
+     * Updates the display to show the current speed
+     * @param speed The current speed of the user
+     */
+    private void updateSpeed(double speed) {
+        final TextView speedVal = (TextView) findViewById(R.id.SpeedVal);
+        speedVal.setText(String.format("%.2f", speed));
+    }
+
+    /**
+     * Updates the current estimated mile time
+     * @param currentPace User's current mile time
+     */
+    private void updateCurrentPace(double currentPace) {
+        int minutes = (int)currentPace;
+        int seconds = (int)(((currentPace * 100) % 100) * 0.6);
+        final TextView emtVal = (TextView) findViewById(R.id.emtVal);
+        emtVal.setText(String.format("%d:%02d", minutes, seconds));
+    }
+
+    /**
+     * Updates the current goal mile time
+     * @param goalPace New goal mile time
+     */
+    private void updateGoalPace(double goalPace) {
+        int minutes = (int)goalPace;
+        int seconds = (int)(((goalPace * 100) % 100) * 0.6);
+        final TextView gmtVal = (TextView) findViewById(R.id.gmtVal);
+        gmtVal.setText(String.format("%d:%02d", minutes, seconds));
+    }
+
+    /**
+     * Updates the current pace text
+     * @param paceText indicator for user;s current speed in relation to goal time
+     */
+    private void updatePaceText(String paceText) {
+        final TextView pace = (TextView) findViewById(R.id.paceView);
+        pace.setText(paceText);
+    }
+
+    /**
+     * Checks current pace and assigns appropriate text
+     */
+    private void paceAlert() {
+        if (currentPace > goalPace + MILE_TIME_ERROR) {
+            paceText = "Speed up";
+            vibrator.vibrate(300);
+            try {
+                Thread.sleep(300);
+            } catch (Exception e) {}
+            vibrator.vibrate(300);
+            try {
+                Thread.sleep(300);
+            } catch (Exception e) {}
+            vibrator.vibrate(300);
+        } else if (currentPace < goalPace - MILE_TIME_ERROR) {
+            paceText = "Slow Down";
+            vibrator.vibrate(1000);
+        } else {
+            paceText = "Perfect Pace!";
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -260,9 +222,23 @@ public class SpeedAlarmActivity extends ActionBarActivity {
 
     @Override
     public void onBackPressed() {
-        locationManager.removeUpdates(locationListener);
+        // Terminate the speed calculation service
+        stopService(new Intent(getBaseContext(), SpeedCalculationService.class));
         finish();
         return;
     }
 
+    ServiceConnection speedConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SpeedCalculationBinder  binder = (SpeedCalculationBinder) service;
+            speedCalculator = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isBound = false;
+        }
+    };
 }
---
+++ app/src/main/java/com/example/stropheum/speedcalculatortest/SpeedCalculationService.java
@@ -0,0 +1,146 @@
+package com.example.stropheum.speedcalculatortest;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+public class SpeedCalculationService extends Service {
+
+    final int MILLI_TO_SEC = 1000; // Number of milliseconds in a second
+    final int SEC_TO_HOUR = 3600;  // Number of seconds in an hour
+
+    private final IBinder binder = new SpeedCalculationBinder();
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    boolean firstLocationCheck = true;
+
+    // Tracks distance traveled between location calls
+    double distanceTraveled = 0;
+    double speed = 0;
+
+    public SpeedCalculationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // This service runs until it is stopped
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+
+            // Tracks the longitude and latitude of the previous and current location calls
+            double lonNew, lonOld;
+            double latNew, latOld;
+
+            double startTime = System.currentTimeMillis();
+            double currentTime, timeElapsed;
+
+            public void onLocationChanged(Location location) {
+
+                if (firstLocationCheck) {
+                    // Prime old locations for first distance calculation
+                    latOld = Math.toRadians(location.getLatitude());
+                    lonOld = Math.toRadians(location.getLongitude());
+                    firstLocationCheck = false;
+                }
+
+                latNew = Math.toRadians(location.getLatitude());
+                lonNew = Math.toRadians(location.getLongitude());
+
+                currentTime = System.currentTimeMillis();
+                timeElapsed = currentTime - startTime;
+
+                distanceTraveled += haversine(latOld, lonOld, latNew, lonNew);
+                if (distanceTraveled > 1000) { distanceTraveled = 0; } // Handles start errors
+
+                speed = distanceTraveled / timeElapsed * MILLI_TO_SEC * SEC_TO_HOUR;
+
+                latOld = latNew;
+                lonOld = lonNew;
+
+            }
+            public void onStatusChanged(String Provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        return START_STICKY;
+    }
+
+    /**
+     * Computes distance (in miles) between two coordinates using the haversine formula
+     * @param lat1 latitude  of previous location
+     * @param lon1 longitude of previous location
+     * @param lat2 latitude  of current  location
+     * @param lon2 longitude of current  location
+     * @return distance in miles
+     */
+    private double haversine(double lat1, double lon1, double lat2, double lon2) {
+        final double EARTH_RADIUS_M = 3959;
+
+        double dLon, dLat, a, c, distance;
+
+        // Calculate distance traveled using Haversine formula
+        dLon = lon2 - lon1;
+        dLat = lat2 - lat1;
+
+        a = Math.sin(dLat/2.0) * Math.sin(dLat/2.0) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon/2.0) * Math.sin(dLon/2.0);
+        System.out.println("a = " + a);
+
+        c = 2.0 * Math.atan(Math.sqrt(a));
+        System.out.println("c = " + c);
+        distance = EARTH_RADIUS_M * c;
+
+        return distance;
+    }
+
+    /**
+     * Returns the current calculated speed of the user
+     * @return the current speed in mph format
+     */
+    public double getCurrentSpeed() {
+        return this.speed;
+    }
+
+    /**
+     * Method to check if GPS connection is established
+     * @return true if first location check has been completed
+     */
+    public boolean gpsSignalFound() {
+        return !this.firstLocationCheck;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        locationManager.removeUpdates(locationListener);
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
+    }
+
+    // Binder class that will bind to the workout activities
+    public class SpeedCalculationBinder extends Binder {
+        SpeedCalculationService getService() {
+            return SpeedCalculationService.this;
+        }
+    }
+
+}
